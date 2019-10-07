bc24c87d263ba2b2fe08803a253f91db579b7451
Weihao Ke <weihaoke13@gmail.com>
2019-10-06 18:24:25 -0700

    Updates

---
+++ precache-manifest.01f5996c0cfe0c55133763c9bb34cdfb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e36835b972f9dc92ec60d07ad6b3d418",
+    "url": "/mytshirtapp/index.html"
+  },
+  {
+    "revision": "b4a0b73fcdf738f211eb",
+    "url": "/mytshirtapp/static/css/main.a41948a7.chunk.css"
+  },
+  {
+    "revision": "ab0450a7dfd87e7b3048",
+    "url": "/mytshirtapp/static/js/2.25611c8b.chunk.js"
+  },
+  {
+    "revision": "b4a0b73fcdf738f211eb",
+    "url": "/mytshirtapp/static/js/main.0c0844e0.chunk.js"
+  },
+  {
+    "revision": "c244b18cf2d5cdbcc4cf",
+    "url": "/mytshirtapp/static/js/runtime~main.52def38a.js"
+  }
+]);
